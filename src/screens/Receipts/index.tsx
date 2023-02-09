79fb0e6ba1c99ddc275d8031e936cb84475be716
FRANCISCO WILLIAM DE OLIVEIRA LIMA <francisco.wolima@senacsp.edu.br>
2023-02-08 22:23:58 -0300

    excluindo images

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,10 +1,11 @@
-import React,{useEffect, useState} from 'react';
-import { FlatList } from 'react-native';
+import React,{useEffect, useState, useCallback} from 'react';
+import { Alert, FlatList } from 'react-native';
 
 import { Container, PhotoInfo } from './styles';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
 import { File, FileProps } from '../../components/File';
+import { useFocusEffect } from '@react-navigation/native';
 
 // import { photosData } from '../../utils/photo.data';
 import storage from '@react-native-firebase/storage';
@@ -14,27 +15,55 @@ export function Receipts() {
 
   const [photos, setPhotos] = useState<FileProps[]>([])
   const [photoSelected, setPhotoSelected] = useState('');
+  const [photoInfo, setPhotoInfo] = useState('');
+
+
 async function handleShowImage(path:string){
   const urlImage = await storage().ref(path).getDownloadURL();
   setPhotoSelected(urlImage);
+
+
+    //tranzendo info do arquivo 
+  const info = await storage().ref(path).getMetadata();
+  // info.name
+  setPhotoInfo(`Upload realizado ${info.timeCreated}| ${info.size} bytes`);
+}
+
+async function handleDeleteImage(path:string){
+  storage().ref().delete().then(()=>{
+    Alert.alert("Imagem Excluida com sucesso!");
+    fetchImage();
+    setPhotoSelected('');
+    setPhotoInfo('');
+  })
+  .catch((error) => console.log(error));
 }
 
 
 
-
-  useEffect(()=>{
-    storage().ref('images').list().then( result =>{
-      const files: FileProps[] = [];
-
-      result.items.forEach( file =>{
-        files.push({
-          name: file.name,
-          path: file.fullPath,
-        })
-        setPhotos(files)
+async function fetchImage(){
+  storage().ref('images').list().then( result =>{
+    const files: FileProps[] = [];
+  
+    result.items.forEach( file =>{
+      files.push({
+        name: file.name,
+        path: file.fullPath,
       })
+      setPhotos(files)
     })
+  })
+
+}
+  useEffect(()=>{
+    fetchImage();
   },[])
+
+useFocusEffect(
+  useCallback(() =>{
+    fetchImage()
+  },[])
+)
 
   return (
     <Container>
@@ -43,7 +72,7 @@ async function handleShowImage(path:string){
       <Photo uri={photoSelected} />
 
       <PhotoInfo>
-        Informações da foto
+        {photoInfo}
       </PhotoInfo>
 
       <FlatList
@@ -53,7 +82,7 @@ async function handleShowImage(path:string){
           <File
             data={item}
             onShow={() => handleShowImage(item.path)}
-            onDelete={() => { }}
+            onDelete={() => handleDeleteImage(item.path)}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
